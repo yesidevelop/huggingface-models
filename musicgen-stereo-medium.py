b5bec216c93e8a149d2d6b9d9e19d34c051f18b2
Ali Mehdi <alimehdi.official@gmail.com>
2026-01-01 21:48:23 +0500

    adding music

--- musicgen-stereo-medium.py
+++ musicgen-stereo-medium.py
@@ -4,6 +4,6 @@ from transformers import pipeline
 
 synthesiser = pipeline("text-to-audio", "facebook/musicgen-stereo-medium", device="cuda:0", torch_dtype=torch.float16)
 
-music = synthesiser("lo-fi music with a soothing melody", forward_params={"max_new_tokens": 256})
+music = synthesiser("Calm ambient yoga music, soft pads, slow tempo, peaceful, cinematic, relaxing atmosphere", forward_params={"max_new_tokens": 256})
 
 sf.write("musicgen_out.wav", music["audio"][0].T, music["sampling_rate"])

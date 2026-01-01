9f955f0267aeda77986780c9d12b0b6ba7721efa
Ali Mehdi <alimehdi.official@gmail.com>
2026-01-01 21:57:53 +0500

    adding music

--- musicgen-stereo-medium.py
+++ musicgen-stereo-medium.py
@@ -3,6 +3,6 @@ import scipy
 
 synthesiser = pipeline("text-to-audio", "facebook/musicgen-large")
 
-music = synthesiser("Calm ambient yoga music, soft pads, slow tempo, peaceful, cinematic, relaxing atmosphere", forward_params={"do_sample": True})
+music = synthesiser("Calm ambient yoga music with breathy bamboo flute melody, soft evolving synth pads, slow tempo, no percussion, peaceful, spiritual, spacious, cinematic", forward_params={"do_sample": True})
 
 scipy.io.wavfile.write("musicgen_out.wav", rate=music["sampling_rate"], data=music["audio"])

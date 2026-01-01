6ca2584a69d31cc8b8b0af4d0a5ab3f301483619
Ali Mehdi <alimehdi.official@gmail.com>
2026-01-01 21:52:07 +0500

    adding music

--- musicgen-stereo-medium.py
+++ musicgen-stereo-medium.py
@@ -1,9 +1,8 @@
-import torch
-import soundfile as sf
 from transformers import pipeline
+import scipy
 
-synthesiser = pipeline("text-to-audio", "facebook/musicgen-stereo-medium", device="cuda:0", torch_dtype=torch.float16)
+synthesiser = pipeline("text-to-audio", "facebook/musicgen-large")
 
-music = synthesiser("Calm ambient yoga music, soft pads, slow tempo, peaceful, cinematic, relaxing atmosphere", forward_params={"max_new_tokens": 256})
+music = synthesiser("Calm ambient yoga music, soft pads, slow tempo, peaceful, cinematic, relaxing atmosphere", forward_params={"do_sample": True})
 
-sf.write("musicgen_out.wav", music["audio"][0].T, music["sampling_rate"])
+scipy.io.wavfile.write("musicgen_out.wav", rate=music["sampling_rate"], data=music["audio"])

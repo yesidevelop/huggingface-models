471d8c88dd509934cfce54067a4747a0d4175441
Ali Mehdi <alimehdi.official@gmail.com>
2026-01-02 16:41:22 +0500

    up

--- musicgen-stereo-medium.py
+++ musicgen-stereo-medium.py
@@ -4,10 +4,11 @@ import scipy
 synthesiser = pipeline("text-to-audio", "facebook/musicgen-large")
 
 prompt = (
-    "Playful, whimsical, and upbeat orchestral music, "
-    "with lively piano, pizzicato strings, marimba, "
-    "light percussion, cartoonish, fun, cinematic, "
-    "animated movie soundtrack style"
+    "Whimsical and magical Pied Piper style music, "
+    "playful melody led by pan flute and piccolo, "
+    "light harp and woodwinds, fast tempo, "
+    "airy, cheerful, mischievous, fairy-tale soundtrack, "
+    "cinematic and enchanting, lively folk-style rhythm"
 )
 music = synthesiser(prompt, forward_params={"do_sample": True, "max_new_tokens": 800})
 

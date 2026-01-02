4f76b9f4d074a3bf3cb9eb8170e6d9b9d997481b
Ali Mehdi <alimehdi.official@gmail.com>
2026-01-02 16:30:46 +0500

    updated

--- musicgen-stereo-medium.py
+++ musicgen-stereo-medium.py
@@ -3,6 +3,12 @@ import scipy
 
 synthesiser = pipeline("text-to-audio", "facebook/musicgen-large")
 
-music = synthesiser("Calm ambient yoga music with breathy bamboo flute melody, soft evolving synth pads, slow tempo, no percussion, peaceful, spiritual, spacious, cinematic", forward_params={"do_sample": True, "max_new_tokens": 400})
+prompt = (
+    "Playful, whimsical, and upbeat orchestral music, "
+    "with lively piano, pizzicato strings, marimba, "
+    "light percussion, cartoonish, fun, cinematic, "
+    "animated movie soundtrack style"
+)
+music = synthesiser(prompt, forward_params={"do_sample": True, "max_new_tokens": 800})
 
 scipy.io.wavfile.write("musicgen_out.wav", rate=music["sampling_rate"], data=music["audio"])

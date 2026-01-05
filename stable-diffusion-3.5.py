4f97474283e6b2d296d5c1c544690faccff23e61
Ali Mehdi <alimehdi.official@gmail.com>
2026-01-05 13:37:25 +0500

    updated

---
+++ stable-diffusion-3.5.py
@@ -0,0 +1,12 @@
+import torch
+from diffusers import StableDiffusion3Pipeline
+
+pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large", torch_dtype=torch.bfloat16)
+pipe = pipe.to("cuda")
+
+image = pipe(
+    "A capybara holding a sign that reads Hello World",
+    num_inference_steps=28,
+    guidance_scale=3.5,
+).images[0]
+image.save("capybara.png")

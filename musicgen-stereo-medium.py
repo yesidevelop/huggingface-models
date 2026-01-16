5540aae4f3fe2aa993d63d8c1f706f6673cd53e5
Ali Mehdi <alimehdi.official@gmail.com>
2026-01-16 23:15:27 +0500

    up

--- musicgen-stereo-medium.py
+++ musicgen-stereo-medium.py
@@ -6,6 +6,6 @@ synthesiser = pipeline("text-to-audio", "facebook/musicgen-large")
 prompt = (
     "Dark pop instrumental, steady 808 heartbeat kick drum, crisp closed hi-hats, soft suspenseful synth drones, muted plucked guitar, minor key, 95 BPM, atmospheric and tense, rhythmic but quiet."
 )
-music = synthesiser(prompt, forward_params={"do_sample": True, "max_new_tokens": 3200})
+music = synthesiser(prompt, forward_params={"do_sample": True, "max_new_tokens": 1600})
 
 scipy.io.wavfile.write("musicgen_out.wav", rate=music["sampling_rate"], data=music["audio"])

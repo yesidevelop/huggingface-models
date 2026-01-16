81bc251b25a1b8ea70f1496093ebcd793262080f
Ali Mehdi <alimehdi.official@gmail.com>
2026-01-16 18:01:16 +0500

    update

--- bark.py
+++ bark.py
@@ -1,15 +1,8 @@
-from transformers import AutoProcessor, AutoModel
+from transformers import pipeline
 import scipy
 
-processor = AutoProcessor.from_pretrained("suno/bark-small")
-model = AutoModel.from_pretrained("suno/bark-small")
+synthesiser = pipeline("text-to-speech", "suno/bark-small")
 
-inputs = processor(
-    text=["Hello, my name is Suno. And, uh — and I like pizza. [laughs] But I also have other interests such as playing tic tac toe."],
-    return_tensors="pt",
-)
+speech = synthesiser("Hello, my dog is cooler than you!", forward_params={"do_sample": True})
 
-speech_values = model.generate(**inputs, do_sample=True)
-
-sampling_rate = model.config.sample_rate
-scipy.io.wavfile.write("bark_out.wav", rate=sampling_rate, data=speech_values.cpu().numpy().squeeze())
+scipy.io.wavfile.write("bark_out.wav", rate=speech["sampling_rate"], data=speech["audio"])

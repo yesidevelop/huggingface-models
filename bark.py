c1580e6c489423ec1fbaaebae21ffebc28ba5e69
Ali Mehdi <alimehdi.official@gmail.com>
2026-01-16 18:08:24 +0500

    update

--- bark.py
+++ bark.py
@@ -1,22 +1,9 @@
-from transformers import pipeline
-import numpy as np
-import scipy.io.wavfile as wavfile
-
-synthesiser = pipeline("text-to-speech", "suno/bark-small")
-
-speech = synthesiser(
-    "Hello, my dog is cooler than you!",
-    forward_params={"do_sample": True}
-)
-
-audio = speech["audio"]
-sampling_rate = speech["sampling_rate"]
-
-# Ensure numpy array
-audio = np.asarray(audio)
-
-# Normalize to int16 PCM
-audio = audio / np.max(np.abs(audio))
-audio_int16 = (audio * 32767).astype(np.int16)
-
-wavfile.write("bark_out.wav", sampling_rate, audio_int16)
+import scipy
+from transformers import AutoProcessor, BarkModel
+processor = AutoProcessor.from_pretrained("suno/bark-small")
+model = BarkModel.from_pretrained("suno/bark-small")
+inputs = processor(""" Hello, my dog is cooler than you! """, voice_preset="v2/en_speaker_5")
+audio_array = model.generate(**inputs)
+audio_array = audio_array.cpu().numpy().squeeze()
+sample_rate = model.generation_config.sample_rate
+scipy.io.wavfile.write("bark_out.wav",rate=sample_rate, data=audio_array)

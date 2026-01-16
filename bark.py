fcdc26c666abd8b2e9b332dc7e4c76a99cd0b03b
Ali Mehdi <alimehdi.official@gmail.com>
2026-01-16 18:11:54 +0500

    update

--- bark.py
+++ bark.py
@@ -1,7 +1,7 @@
 import scipy
 from transformers import AutoProcessor, BarkModel
-processor = AutoProcessor.from_pretrained("suno/bark-small")
-model = BarkModel.from_pretrained("suno/bark-small")
+processor = AutoProcessor.from_pretrained("suno/bark")
+model = BarkModel.from_pretrained("suno/bark")
 inputs = processor(""" Hello, my dog is cooler than you! """, voice_preset="v2/en_speaker_5")
 audio_array = model.generate(**inputs)
 audio_array = audio_array.cpu().numpy().squeeze()

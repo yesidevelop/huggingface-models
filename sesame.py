2ffbadaf4c0021eeba0b92e8f7776bc41f80abd6
Ali Mehdi <alimehdi.official@gmail.com>
2026-01-17 00:47:44 +0500

    up

--- sesame.py
+++ sesame.py
@@ -9,12 +9,12 @@ processor = AutoProcessor.from_pretrained(model_id)
 model = CsmForConditionalGeneration.from_pretrained(model_id, device_map=device)
 
 # prepare the inputs
-text = "[0]Hello from Sesame." # `[0]` for speaker id 0
+text = "[0]That is actually hilarious, haha! I can't believe it." # `[0]` for speaker id 0
 inputs = processor(text, add_special_tokens=True).to(device)
 
 # another equivalent way to prepare the inputs
 conversation = [
-    {"role": "0", "content": [{"type": "text", "text": "Hi [laugh]"}]},
+    {"role": "0", "content": [{"type": "text", "text": "That is actually hilarious, haha! I can't believe it."}]},
 ]
 inputs = processor.apply_chat_template(
     conversation,

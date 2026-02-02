0f9410e99beec3a9de3f4de88df4c1ad206e657d
Ali Mehdi <alimehdi.official@gmail.com>
2026-02-02 09:23:26 +0500

    prompt updated

--- musicgen-stereo-medium.py
+++ musicgen-stereo-medium.py
@@ -6,6 +6,6 @@ synthesiser = pipeline("text-to-audio", "facebook/musicgen-large")
 prompt = (
     "[Instrumental] A slow, cinematic fairy tale theme. Begins with a delicate chime harp and celesta duet. A soulful solo violin enters with a sweeping melody, supported by an ethereal female choir and light flute trills. Orchestral, magical, 60 BPM, high-fidelity, soaring and emotional."
 )
-music = synthesiser(prompt, forward_params={"do_sample": True, "max_new_tokens": 3200})
+music = synthesiser(prompt, forward_params={"do_sample": True, "max_new_tokens": 2000})
 
 scipy.io.wavfile.write("musicgen_out.wav", rate=music["sampling_rate"], data=music["audio"])
